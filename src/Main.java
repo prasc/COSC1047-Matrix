601a4b0ea77f2109dbc8e2e92d9841a7b2bf8a2d
prashathc <prashathc@gmail.com>
2020-10-08 20:35:22 -0400

    initial

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the array size n: ");
+
+        int size = input.nextInt();
+
+        ArrayList<Integer> matrix = new ArrayList<>();
+
+        
+    }
+}
